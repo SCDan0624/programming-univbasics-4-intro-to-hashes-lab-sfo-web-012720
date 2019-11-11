bddb23b99327479f108b2f72e06e7c1bf46fd5d6
Daniel Mayle <dmayle012@gmail.com>
2019-11-11 18:54:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,11 +15,13 @@ def pioneer
 end
 
 def id_generator
-  
+  new_hash_3 = {
+    id:6
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  
 end
 
 def read_from_hash(hash, key)
